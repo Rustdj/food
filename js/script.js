066b0881510f85730115f4b25f03183e6366dd90
Rustdj <81041717+Rustdj@users.noreply.github.com>
2022-04-19 18:22:00 +0300

    Updates

---
+++ js/script.js
@@ -0,0 +1,470 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+// Tabs
+    const tabs = document.querySelectorAll('.tabheader__item'),
+          tabsContent = document.querySelectorAll('.tabcontent'),
+          tabsParent = document.querySelector('.tabheader__items');
+
+          function hideTabContent() {
+              tabsContent.forEach(item => {
+                  item.style.display = 'none';
+
+              });
+
+              tabs.forEach(item => {
+                  item.classList.remove('tabheader__item_active');
+              });
+
+
+          }
+          function showTabContent(i = 0) {
+              tabsContent[i].style.display = 'block';
+              tabs[i].classList.add('tabheader__item_active');
+          }
+
+          hideTabContent();
+          showTabContent();
+
+
+          tabsParent.addEventListener('click', (event) => {
+              const target = event.target;
+
+              if (target && target.classList.contains('tabheader__item')) {
+                  tabs.forEach((item, i) => {
+                      if (target == item) {
+                        hideTabContent();
+                        showTabContent(i);
+                      }
+                  });
+              }
+          });
+
+          // Timer
+
+    const deadline = '2021-08-20';
+
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+            days = Math.floor( (t/(1000*60*60*24)) ),
+            seconds = Math.floor( (t/1000) % 60 ),
+            minutes = Math.floor( (t/1000/60) % 60 ),
+            hours = Math.floor( (t/(1000*60*60) % 24) );
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function getZero(num){
+        if (num >= 0 && num < 10) { 
+            return '0' + num;
+        } else {
+            return num;
+        }
+    }
+
+    function setClock(selector, endtime) {
+
+        const timer = document.querySelector(selector),
+            days = timer.querySelector("#days"),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('.timer', deadline);
+
+
+
+
+    // Modal
+
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+        modal = document.querySelector('.modal');
+
+    function openModal() {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow = 'hidden';
+        clearInterval(modalTimerId);
+    }
+
+    modalTrigger.forEach(btn => {
+        btn.addEventListener('click', openModal);
+    });
+
+    function closeModal() {
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        // Либо вариант с toggle - но тогда назначить класс в верстке
+        document.body.style.overflow = '';
+    }
+    
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal || e.target.getAttribute('data-close') == '') {
+            closeModal();
+        }
+    });
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code === "Escape" && modal.classList.contains('show')) { 
+            closeModal();
+        }
+    });
+
+    // автоматическое появление мщдального окна
+
+    const modalTimerId = setTimeout(openModal, 5000);
+
+    function showModalByScroll() {
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll);
+        }
+    }
+
+    window.addEventListener('scroll', showModalByScroll);
+
+    // Используем классы для картоек
+
+class MenuCard {
+    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
+        this.src = src;
+        this.alt = alt;
+        this.title = title;
+        this.descr = descr;
+        this.price = price;
+        this.classes = classes;
+        this.parent = document.querySelector(parentSelector);
+        this.transfer = 27;
+        this.changeToUAH();
+    }
+
+    changeToUAH() {
+        this.price = this.price * this.transfer;
+    }
+
+    render() {
+        const element = document.createElement('div');
+        this.classes.forEach(className => element.classList.add(className));
+        element.innerHTML = ` 
+            <img src=${this.src} alt=${this.alt}>
+            <h3 class="menu__item-subtitle">${this.title}</h3>
+            <div class="menu__item-descr">${this.descr}</div>
+            <div class="menu__item-divider"></div>
+            <div class="menu__item-price">
+            <div class="menu__item-cost">Цена:</div>
+            <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+            </div>
+            
+                `;
+                this.parent.append(element);
+    }
+}
+
+            new MenuCard(
+            "img/tabs/vegy.jpg",
+            "vegy",
+            'Меню "Фитнес"',
+            'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+            9,
+            '.menu .container',
+            'menu__item'
+            ).render();
+
+            new MenuCard(
+            "img/tabs/elite.jpg",
+            "elite",
+            'Меню “Премиум”',
+            'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+            9,
+            '.menu .container',
+            'menu__item'
+            ).render();
+
+            new MenuCard(
+            "img/tabs/post.jpg",
+            "post",
+            'Меню "Постное"',
+            'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+            9,
+            '.menu .container',
+            'menu__item'
+            ).render();
+
+            // отправдение POST на сервер Forms
+
+            const forms = document.querySelectorAll('form');
+            const message = {
+                loading: 'img/form/spinner.svg',
+                success: 'Спасибо! Скоро мы с вами свяжемся',
+                failure: 'Что-то пошло не так..'
+            };
+
+            forms.forEach(item => {
+                postData(item);
+            });
+
+            const postData = async (url, data) => {
+                const res = await fetch(url, {
+                    method: "POST",
+                        headers: {
+                            'Content-type': 'application/json'
+                        },
+                        body: data
+                });
+
+                return res.json();
+            };
+
+            function bindPostData(form) {
+                form.addEventListener('submit', (e) => {
+                    e.preventDefault();
+
+                    const statusMessage = document.createElement('img');
+                    statusMessage.src = message.loading;
+                    statusMessage.style.cssText = `
+                    display: block;
+                    margin: 0 auto;
+                    `;
+                    form.insertAdjacentElement('afterend', statusMessage);
+                    
+        
+                    const formData = new FormData(form);
+
+                    const object = {};
+                    formData.forEach(function(value, key) {
+                        object[key] = value;
+                    });
+
+                    postData('server.php', JSON.stringify(object))
+                    .then(data => {
+                        console.log(data);
+                        showThanksModal(message.success);
+                        statusMessage.remove();
+                    }).catch(() => {
+                        showThanksModal(message.failure);
+                    }).finally(() => {
+                        form.reset();
+                    });
+
+                });
+            }
+
+
+            function showThanksModal(message) {
+                const prevModalDialog = document.querySelector('.modal__dialog');
+
+                prevModalDialog.classList.add('hide');
+                openModal();
+
+                const thanksModal = document.createElement('div');
+                thanksModal.classList.add('modal__dialog');
+                thanksModal.innerHTML = `
+                    <div class="modal__content">
+                        <div class="modal__close" data-close>x</div>
+                        <div class="modal__title">${message}</div>
+                    </div>
+                `;
+
+                document.querySelector('.modal').append(thanksModal);
+                setTimeout(() => {
+                    thanksModal.remove();
+                    prevModalDialog.classList.add('show');
+                    prevModalDialog.classList.remove('hide');
+                    closeModal();
+                }, 4000);
+            }
+
+            fetch('db.json')
+                .then(data => data.json())
+                .then(res => console.log(res));
+
+
+});
+
+
+
+// Fetch API----------------------
+    // позволяет общаться с сервером, построена на промисах
+    //fetch('https://jsonplaceholder.typicode.com/posts', {
+            //method: "POST",
+            //body: JSON.stringify({name: 'Alex'}),
+            //headers: {
+               // 'Content-type': 'application/json'
+            //}
+    //})
+    //.then(response => response.json())
+    //.then(json => console.log(json));
+
+    // json-server - сервер по работе с json-файлами когда мы ее можем использовать как маленькую базу данных!
+
+// контекст вызова this
+
+/*function showThis() {
+    console.log(this);
+    function sum();
+    console.log(this);
+        return this.a + this.b
+}
+
+
+showThis();
+
+const obj = {
+    a: 20,
+    b: 15,
+    sum: function() {
+        console.log(this);
+    }
+
+};
+obj.sum();
+
+function User(name, id) {
+    this.name = name;
+    this.id = id;
+    this.human = human;
+}
+let ivan = new User('Ivan', 23);
+
+// ":":":":":"
+
+function sayName(surname) {
+    console.log(this);
+    console.log(this.name + surname);
+}
+
+const user = {
+    name: 'Jone'
+};
+
+sayName.call(user, 'Smit');
+sayName.apply(user, ['Smit']);
+
+function count(num) {
+    return this*num;
+}
+
+const double = count.bind(2);
+console.log(double(3));
+console.log(double(13));
+
+//practica
+
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', function() {
+    //console.log(this);
+    this.style.backgroundColor = 'red';
+});
+
+const obj = {
+    num = 5,
+    sayNumber: function() {
+        const say = () => {
+            console.log(this);
+        };
+
+        say();
+    }
+};
+
+obj.sayNumber();
+
+
+const double = (a) => {
+    return a * 2;
+};
+// ili vottakoi sintaksis
+
+const double = a => a * 2;
+
+// standart ES6
+// klassi
+
+class Rectangle {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    calcArea() {
+        return this.height * this.width;
+    }
+}
+
+const div = new ColorRectangleWithText(25, 10, 'Hello World', 'red');
+
+class ColorRectangleWithText extends Rectangle {
+    constructor(height, width, text, bgColor) {
+        super(height, width);
+        this.text = text;
+        this.bgColor = bgColor;
+    }
+
+    showMyProps() {
+        console.log(`Текст: ${this.text}, цвет: ${this.bgColor}`);
+
+        div.showMyProps();
+        console.log(div.calcArea());
+    }
+}
+
+const square = new Rectangle(10, 10);
+const long = new Rectangle(20, 100);
+
+console.log(long.calcArea());
+console.log(square.calcArea());*/
+
+// Rest оператор и параметры по умолчанию ES6
+
+/*const log = (a, b, ...rest) {
+    console.log(a, b, rest);
+}
+
+log('basic', 'rest', 'operator', 'usage')*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
